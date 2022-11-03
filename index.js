227646a7db4ae06156d40c9fea74d558304f2c63
jhara islam <103064101+jharaislam19@users.noreply.github.com>
2022-11-03 08:32:35 +0600

    add more function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -25,7 +25,56 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
   try{
+   //ALL SERVICES GET
      const servicesCollection = client.db('geniusCar').collection('services');
+     const ordersCollection = client.db('geniusCar').collection('orders');
+     app.get('/services', async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query)
+      const services = await cursor.toArray();
+      res.send(services);
+     })
+
+//SINGLE SERVICE GET
+app.get('/services/:id', async (req, res) => {
+   const id = req.params.id;
+   const query = {_id: ObjectId(id)};
+   const service = await servicesCollection.findOne(query);
+   res.send(service); 
+ })
+
+//ORDERS API GET
+app.get('/orders', async (req, res) => {
+   console.log(req.query);
+   //const query = {};
+   let query = {};
+   if(req.query.email){
+     query = {
+        email: req.query.email
+     }
+   }
+
+   const cursor = ordersCollection.find(query);
+   const orders = await cursor.toArray();
+   res.send(orders);
+})
+
+
+//ORDERS API CREATE
+app.post('/orders', async (req, res) => {
+    const order = req.body;
+    const result = await ordersCollection.insertOne(order);
+    res.send(result);
+});
+
+//DELETE FUNCTION
+app.delete('/orders/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = {_id: ObjectId(id)};
+  const result = await ordersCollection.deleteOne(query);
+  res.send(result);
+}
+
   }
   finally{
 
